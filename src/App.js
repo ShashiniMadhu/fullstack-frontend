c03dcedc57e10dd2669742af9500fd928904570c
ShashiniMadhu <shashinimadushika413@gmail.com>
2025-05-24 20:00:59 +0530

    Retriew data to the frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios';
+
 import Navbar from './layout/Navbar';
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
     </div>
   );
 }
